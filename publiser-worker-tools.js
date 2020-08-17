eabfc93621e556bbcd2243ff62748728dbd4ad43
unknown <behrooz.arabzade@gmail.com>
2020-08-17 17:59:26 +0430

    debugging

--- publiser-worker-tools.js
+++ publiser-worker-tools.js
@@ -17,16 +17,10 @@ function existsAsync(path) {
   })
 }
 
-/**
- * Executes a shell command and return it as a Promise.
- * @param cmd {string}
- * @return {Promise<string>}
- */
 function execShellCommand(cmd) {
-    const exec = require('child_process').exec;
     return new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
-            let success = !!error;
+            let success = !(error);
             resolve({success, stdout, stderr, error});
         });
     });
