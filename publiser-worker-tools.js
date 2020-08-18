94a8b3ea94b3ac5695a4d0c819cf004239db3a24
unknown <behrooz.arabzade@gmail.com>
2020-08-18 20:10:23 +0430

    debugging

--- publiser-worker-tools.js
+++ publiser-worker-tools.js
@@ -3,7 +3,9 @@ var Promise = require("bluebird");
 let ncpAsync = Promise.promisify(require('ncp').ncp);
 let crypto = require('crypto');
 const exec = require('child_process').exec;
-const { spawn } = require("child_process");
+const {
+    spawn
+} = require("child_process");
 let execP = Promise.promisify(exec);
 const getPort = require('get-port');
 const dbTools = require('./dbTools.js');
@@ -14,15 +16,15 @@ const cloneDeep = require('clone-deep');
 let axios = require('axios');
 
 function existsAsync(path) {
-  return new Promise(function(resolve, reject){
-    fs.exists(path, function(exists){
-      resolve(exists);
+    return new Promise(function (resolve, reject) {
+        fs.exists(path, function (exists) {
+            resolve(exists);
+        })
     })
-  })
 }
 
 function waitForMilis(milis) {
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
         setTimeout(() => {
             resolve();
         }, milis);
@@ -33,7 +35,12 @@ function execShellCommand(cmd, config) {
     return new Promise((resolve, reject) => {
         exec(cmd, config, (error, stdout, stderr) => {
             let success = !(error);
-            resolve({success, stdout, stderr, error});
+            resolve({
+                success,
+                stdout,
+                stderr,
+                error
+            });
         });
     });
 }
@@ -48,26 +55,45 @@ function spawnAsync(cmd, args, options, unref) {
         ls.stdout.on('data', (data) => {
             out += data;
         });
-          
+
         ls.stderr.on('data', (data) => {
             err += data;
         });
 
         ls.on('error', (error) => {
-            console.log("Spawn Result: ", false, {stdout: out, stderr: err});
+            console.log("Spawn Result: ", false, {
+                stdout: out,
+                stderr: err
+            });
             resolved = true;
-            resolve({success: false, stdout: out, stderr: err, error: error});
+            resolve({
+                success: false,
+                stdout: out,
+                stderr: err,
+                error: error
+            });
         });
 
         ls.on("close", code => {
-            console.log("Spawn Result: ", true, {stdout: out, stderr: err});
+            console.log("Spawn Result: ", true, {
+                stdout: out,
+                stderr: err
+            });
             resolved = true;
-            resolve({success: true, stdout: out, stderr: err});
+            resolve({
+                success: true,
+                stdout: out,
+                stderr: err
+            });
         });
 
         setTimeout(() => {
             if (!resolved)
-                resolve({success: true, stdout: out, stderr: err});
+                resolve({
+                    success: true,
+                    stdout: out,
+                    stderr: err
+                });
         }, 4000);
 
         if (unref)
@@ -76,12 +102,22 @@ function spawnAsync(cmd, args, options, unref) {
 }
 
 let start = async (req, res) => {
-    let {publisherId, publisherDomains, sudoPassword, postgresHost,
-        hasPrivateDomain, publisherBrandName, publisherVersion, expressPort,
-        longProcessId, longProcessUrl} = req.body;
+    let {
+        publisherId,
+        publisherDomains,
+        sudoPassword,
+        postgresHost,
+        hasPrivateDomain,
+        publisherBrandName,
+        publisherVersion,
+        expressPort,
+        longProcessId,
+        longProcessUrl,
+        longProcessToken
+    } = req.body;
 
     let baseFilePath = `/base_files_${publisherVersion}`;
-    
+
     console.log("path: ", __dirname + baseFilePath)
     if (!await existsAsync(__dirname + baseFilePath)) {
         res.status(404).json(
@@ -110,26 +146,28 @@ let start = async (req, res) => {
     res.json(
         new Response(true).json()
     );
-    
+
     try {
         // TODO make it async in safe way
-        if (!fs.existsSync(path)){
+        if (!fs.existsSync(path)) {
             fs.mkdirSync(path);
         }
         console.log("Starting Publisher ...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Starting Publisher ...', 'running', {
-            progress: 2
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Starting Publisher ...', 'running', {
+                progress: 2
+            });
 
         let dbName = `PublisherDB_${publisherId}${
             publisherBrandName? '_' + publisherBrandName: ''
         }`;
-        
+
         /// Database Configs
         console.log("Database Configs ...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Database Configs ...', 'running', {
-            progress: 2
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Database Configs ...', 'running', {
+                progress: 2
+            });
         let initDbResult = await dbTools.initDB(dbName, {
             user: process.env.POSTGRES_USER,
             host: postgresHost,
@@ -140,20 +178,26 @@ let start = async (req, res) => {
 
         if (!initDbResult.success) {
             console.log("Error: ", initDbResult.error);
-            updateLongProcess(longProcessUrl, longProcessId, 'Database init failed !!!', 'failed', {error: initDbResult.error});
-            throw new Error ('Database init failed !!!');
+            updateLongProcess(longProcessUrl, longProcessToken,
+                longProcessId, 'Database init failed !!!', 'failed', {
+                    error: initDbResult.error
+                });
+            throw new Error('Database init failed !!!');
         }
         /// Database Configs
 
         /// Express Configs
         console.log("Express Configs ...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Express Configs ...', 'running', {
-            progress: 8
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Express Configs ...', 'running', {
+                progress: 8
+            });
         await ncpAsync(expressPath, newExpressPath);
         await waitForMilis(1000);
 
-        let freePort = await getPort({port: getPort.makeRange(4000, 4999)});
+        let freePort = await getPort({
+            port: getPort.makeRange(4000, 4999)
+        });
 
         let data = await fsPromises.readFile(dotEnvExpressPath, 'utf8');
         data = data
@@ -169,21 +213,23 @@ let start = async (req, res) => {
         await fsPromises.writeFile(dotEnvExpressPath, data, 'utf8');
 
         console.log("Express Configs npm install ...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Express Configs npm install ...', 'running', {
-            progress: 9
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Express Configs npm install ...', 'running', {
+                progress: 9
+            });
         let command = 'npm install';
         let installResult = await execShellCommand(command, {
             cwd: newExpressPath,
             env: expressDotEnvObject
         });
-        
+
         if (!installResult.success) {
             console.log("Error: ", installResult.error);
-            updateLongProcess(longProcessUrl, longProcessId, 'Installing Express failed !!!', 'failed', {
-                error: installResult.error
-            });
-            throw new Error ('Installing Express failed !!!');
+            updateLongProcess(longProcessUrl, longProcessToken,
+                longProcessId, 'Installing Express failed !!!', 'failed', {
+                    error: installResult.error
+                });
+            throw new Error('Installing Express failed !!!');
         }
 
         // killing old express port
@@ -198,31 +244,34 @@ let start = async (req, res) => {
         });
 
         console.log(`Express Configs npm run start port ${freePort} ...`);
-        updateLongProcess(longProcessUrl, longProcessId, 'Express Configs npm run start ...', 'running', {
-            progress: 20
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Express Configs npm run start ...', 'running', {
+                progress: 20
+            });
         // TODO can change with forever or pm2
         command = 'npm';
         let startResult = await spawnAsync(command, ['run', 'start'], {
             cwd: newExpressPath,
-            detached:true,
+            detached: true,
             env: newEnv
         }, true);
-        
+
         if (!startResult.success) {
-            console.log("Error: ",startResult.stdout, startResult.error);
-            updateLongProcess(longProcessUrl, longProcessId, 'Running Express failed !!!', 'failed', {
-                error: startResult.error
-            });
-            throw new Error ('Running Express failed !!!');
+            console.log("Error: ", startResult.stdout, startResult.error);
+            updateLongProcess(longProcessUrl, longProcessToken,
+                longProcessId, 'Running Express failed !!!', 'failed', {
+                    error: startResult.error
+                });
+            throw new Error('Running Express failed !!!');
         }
         /// Express Configs
 
         /// Client Configs
         console.log("Client Configs ...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Client Configs ...', 'running', {
-            progress: 40
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Client Configs ...', 'running', {
+                progress: 40
+            });
         await ncpAsync(clientPath, newClientProjectPath);
         await waitForMilis(1000);
 
@@ -246,45 +295,50 @@ let start = async (req, res) => {
         data = data
             .replace(/{homepage}/g, `/publisher_${publisherId}/client`);
         await fsPromises.writeFile(clientPackagePath, data, 'utf8');
-        
+
         console.log("Client Configs npm install ...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Client Configs npm install ...', 'running', {
-            progress: 50
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Client Configs npm install ...', 'running', {
+                progress: 50
+            });
         command = 'npm install';
         let installClientResult = await execShellCommand(command, {
             cwd: newClientProjectPath
         });
-         
+
         if (!installClientResult.success) {
             console.log("Error: ", installClientResult.error);
-            updateLongProcess(longProcessUrl, longProcessId, 'Installing client failed !!!', 'failed', {
-                error: installClientResult.error
-            });
-            throw new Error ('Installing client failed !!!');
+            updateLongProcess(longProcessUrl, longProcessToken,
+                longProcessId, 'Installing client failed !!!', 'failed', {
+                    error: installClientResult.error
+                });
+            throw new Error('Installing client failed !!!');
         }
 
         console.log("Client Configs npm run build ...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Client Configs npm run build ...', 'running', {
-            progress: 70
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Client Configs npm run build ...', 'running', {
+                progress: 70
+            });
         command = 'npm run build';
         let buildResult = await execShellCommand(command, {
             cwd: newClientProjectPath
         });
-        
+
         if (!buildResult.success) {
             console.log("Error: ", buildResult.error);
-            updateLongProcess(longProcessUrl, longProcessId, 'Building client failed !!!', 'failed', {
-                error: buildResult.error
-            });
-            throw new Error ('Building client failed !!!');
+            updateLongProcess(longProcessUrl, longProcessToken,
+                longProcessId, 'Building client failed !!!', 'failed', {
+                    error: buildResult.error
+                });
+            throw new Error('Building client failed !!!');
         }
 
         console.log("Client Configs copying builded files...");
-        updateLongProcess(longProcessUrl, longProcessId, 'Client Configs copying builded files...', 'running', {
-            progress: 90
-        });
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Client Configs copying builded files...', 'running', {
+                progress: 90
+            });
         await ncpAsync(clientGeneratedBuildPath, newClientBuildPath);
         await waitForMilis(1000);
         /// Client Configs
@@ -292,29 +346,31 @@ let start = async (req, res) => {
         /// NginX Configs
         if (hasPrivateDomain) {
             console.log("NginX Configs ...");
-            updateLongProcess(longProcessUrl, longProcessId, 'NginX Configs ...', 'running', {
-                progress: 95
-            });
+            updateLongProcess(longProcessUrl, longProcessToken,
+                longProcessId, 'NginX Configs ...', 'running', {
+                    progress: 95
+                });
             data = await fsPromises.readFile(nginxConfPath, 'utf8');
             publisherDomains.forEach(async domainData => {
                 data = data
-                .replace(/{publisherId}/g, `publisher_${publisherId}`)
-                .replace(/{serverName}/g, `${domainData.root} ${domainData.subs.join(' ')}`)
-                .replace(/{apiPort}/g, freePort);
-    
+                    .replace(/{publisherId}/g, `publisher_${publisherId}`)
+                    .replace(/{serverName}/g, `${domainData.root} ${domainData.subs.join(' ')}`)
+                    .replace(/{apiPort}/g, freePort);
+
                 nginxSitesPath = `${nginxSitesPath}/${domainData.root}.conf`;
-        
+
                 await fsPromises.writeFile(nginxSitesPath, data, 'utf8');
-                
+
                 command = `echo ${sudoPassword} | sudo -S nginx reload`;
-        
+
                 let nginxResult = await execShellCommand(command);
-                
+
                 if (!nginxResult.success) {
-                    updateLongProcess(longProcessUrl, longProcessId, 'Nginx config error !!!', 'failed', {
-                        error: nginxResult.error
-                    });
-                    throw new Error ('Nginx config error !!!');
+                    updateLongProcess(longProcessUrl, longProcessToken,
+                        longProcessId, 'Nginx config error !!!', 'failed', {
+                            error: nginxResult.error
+                        });
+                    throw new Error('Nginx config error !!!');
                 }
             });
         } else {
@@ -323,26 +379,35 @@ let start = async (req, res) => {
         /// NginX Configs
 
         console.log("Finish, Publisher express and client configed successfully.");
-        updateLongProcess(longProcessUrl, longProcessId, 'Finish, Publisher express and client configed successfully.',
-            'complete', {expressPort: freePort, progress: 100});
-    } 
-    catch (error) {
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, 'Finish, Publisher express and client configed successfully.',
+            'complete', {
+                expressPort: freePort,
+                progress: 100
+            });
+    } catch (error) {
         console.log("Configing Error", error);
-        updateLongProcess(longProcessUrl, longProcessId, `Configing Error`, 'failed', {error: error});
-    } 
+        updateLongProcess(longProcessUrl, longProcessToken,
+            longProcessId, `Configing Error`, 'failed', {
+                error: error
+            });
+    }
 };
 
-function updateLongProcess(longProcessUrl, longProcessId, status, state, metaData) {
+function updateLongProcess(longProcessUrl, longProcessToken, longProcessId, status, state, metaData) {
     let config = {
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${longProcessToken}`
         }
     };
 
     axios.post(longProcessUrl, {
-        longProcessId, status, state, metaData
-    }, config).then(res => {
-    }).catch(error => {
+        longProcessId,
+        status,
+        state,
+        metaData
+    }, config).then(res => {}).catch(error => {
         console.log("update long process error: ", error);
     })
 };
@@ -353,4 +418,6 @@ let update = async (req, res) => {
     // TODO 2. 
 };
 
-module.exports = {start};
+module.exports = {
+    start
+};
